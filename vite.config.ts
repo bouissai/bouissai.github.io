f68a3809f32154acc22874f5a319df8d4271c6a6
bouissai <bouissailyass@gmail.com>
2025-11-07 08:53:46 +0100

    Update Vite configuration to dynamically set base path for GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,17 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 import svgLoader from 'vite-svg-loader'
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true'
+const base = isGitHubActions
+  ? repoName.endsWith('.github.io')
+    ? '/'
+    : `/${repoName}/`
+  : '/'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base,
   plugins: [
     vue(),
     vueDevTools(),
